5b42cc81c4bf1b02f1a30457c69acf67821861da
Lukas Kurz <lukas@shortdev.de>
2022-05-11 09:32:36 +0200

    Fixed titlebar

--- QrCodeGenerator/MainPage.xaml.cs
+++ QrCodeGenerator/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using QrCodeGenerator.Pages.Content;
 using Windows.ApplicationModel.Core;
 using Windows.UI;
 using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace QrCodeGenerator
@@ -14,7 +15,7 @@ namespace QrCodeGenerator
             this.InitializeComponent();
 
             var titleBar = ApplicationView.GetForCurrentView().TitleBar;
-            
+
             titleBar.ButtonBackgroundColor = Colors.Transparent;
             titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
 
@@ -22,9 +23,46 @@ namespace QrCodeGenerator
             var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
             coreTitleBar.ExtendViewIntoTitleBar = true;
 
+            CoreApplication.GetCurrentView().TitleBar.LayoutMetricsChanged += (s, e) => UpdateAppTitle(s);
+
             // Set XAML element as a draggable region.
-            // Window.Current.SetTitleBar(AppTitleBar);
+            Window.Current.SetTitleBar(AppTitleBar);
         }
+
+        #region UI
+        // https://github.com/microsoft/WinUI-Gallery/blob/master/XamlControlsGallery/Navigation/NavigationRootPage.xaml.cs
+        void UpdateAppTitle(CoreApplicationViewTitleBar coreTitleBar)
+        {
+            //ensure the custom title bar does not overlap window caption controls
+            Thickness currMargin = AppTitleBar.Margin;
+            AppTitleBar.Margin = new Thickness(currMargin.Left, currMargin.Top, coreTitleBar.SystemOverlayRightInset, currMargin.Bottom);
+        }
+
+        private void NavigationViewControl_DisplayModeChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewDisplayModeChangedEventArgs args)
+        {
+            Thickness currMargin = AppTitleBar.Margin;
+            if (sender.DisplayMode == Microsoft.UI.Xaml.Controls.NavigationViewDisplayMode.Compact)
+            {
+                AppTitleBar.Margin = new Thickness(sender.CompactPaneLength, currMargin.Top, currMargin.Right, currMargin.Bottom);
+            }
+            else
+            {
+                AppTitleBar.Margin = new Thickness(sender.OpenPaneLength, currMargin.Top, currMargin.Right, currMargin.Bottom);
+            }
+        }
+
+        private void NavigationViewControl_PaneOpened(Microsoft.UI.Xaml.Controls.NavigationView sender, object args)
+        {
+            Thickness currMargin = AppTitleBar.Margin;
+            AppTitleBar.Margin = new Thickness(sender.OpenPaneLength, currMargin.Top, currMargin.Right, currMargin.Bottom);
+        }
+
+        private void NavigationViewControl_PaneClosed(Microsoft.UI.Xaml.Controls.NavigationView sender, object args)
+        {
+            Thickness currMargin = AppTitleBar.Margin;
+            AppTitleBar.Margin = new Thickness(sender.CompactPaneLength, currMargin.Top, currMargin.Right, currMargin.Bottom);
+        }
+        #endregion
 
         private void NavigationView_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
         {
